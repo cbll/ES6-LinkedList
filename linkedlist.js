f5ffcc6981de4cb5ebfcb6e63a1f547b9b277395
cbll <bjarkelind@gmail.com>
2017-06-02 23:35:34 +0200

    Fix brackets

--- linkedlist.js
+++ linkedlist.js
@@ -13,24 +13,38 @@ class LinkedList {
 
     addToHead = (value) => {
         const newNode = new Node(value, this.head, null);
-        if (this.head) this.head.prev = newNode;
-        else this.tail = newNode;
+        if (this.head){
+            this.head.prev = newNode;
+        }
+        else{
+            this.tail = newNode;
+        }
         this.head = newNode;
     };
 
     addToTail = (value) => {
         const newNode = new Node(value, null, this.tail);
-        if (this.tail) this.tail.next = newNode;
-        else this.head = newNode;
+        if (this.tail){
+            this.tail.next = newNode;
+        }
+        else {
+            this.head = newNode;
+        }
         this.tail = newNode;
     };
 
     removeHead = () => {
-        if (!this.head) return null;
+        if (!this.head){
+            return null;
+        }
         const val = this.head.value;
         this.head = this.head.next;
-        if (this.head) this.head.prev = null;
-        else this.tail = null;
+        if (this.head){
+            this.head.prev = null;
+        }
+        else {
+            this.tail = null;
+        }
         return val;
     };
 
@@ -38,15 +52,21 @@ class LinkedList {
         if (!this.tail) return null;
         const val = this.tail.value;
         this.tail = this.tail.prev;
-        if (this.tail) this.tail.next = null;
-        else this.head = null;
+        if (this.tail){
+            this.tail.next = null;
+        }
+        else {
+            this.head = null;
+        }
         return val;
     };
 
     search = (searchVal) => {
         const currentNode = this.head;
         while (currentNode) {
-            if (currentNode.value === searchVal) return currentNode.value;
+            if (currentNode.value === searchVal){
+                return currentNode.value;
+            }
             currentNode = currentNode.next;
         }
         return null;
@@ -57,7 +77,9 @@ class LinkedList {
         let currentIndex = 0;
         const currentNode = this.head;
         while(currentNode) {
-            if (currentNode.value === value) indexes.push(currentIndex);
+            if (currentNode.value === value){
+                indexes.push(currentIndex);
+            }
             currentNode = currentNode.next;
             currentIndex++;
         }
